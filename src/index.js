12717293b2ddfad36d772c114e8be25d9d82f47b
Ksenia Volkova <margo.verse@yandex.ru>
2024-10-19 15:29:32 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	const stack = [];
+	const bracketsMap = new Map(bracketsConfig);
+
+	for (let char of str) {
+		if (bracketsMap.has(char)) {
+			if (stack.length > 0 && stack[stack.length - 1] === char && bracketsMap.get(char) === char) {
+				stack.pop();
+			} else {
+				stack.push(char);
+			}
+		} else {
+			if (stack.length === 0 || bracketsMap.get(stack.pop()) !== char) {
+				return false;
+			}
+		}
+	}
+
+	return stack.length === 0;
+};
